d108342cc324e2b0d60d92d0f1d978b619aad7e7
Bill Hilbert <billmakes@protonmail.com>
2022-08-19 13:09:53 -0400

    apply clippy linter suggestions

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+use std::cmp::Ordering;
 mod game;
 use game::Game;
 
 mod graphics;
-use graphics::PlayerAction;
-use graphics::print_game_state_with_hidden_card;
-use graphics::prompt_for_user_action;
 use graphics::print_game_end;
 use graphics::print_game_state;
+use graphics::print_game_state_with_hidden_card;
+use graphics::prompt_for_user_action;
 use graphics::sleep;
+use graphics::PlayerAction;
 
 mod card;
 
@@ -21,7 +22,7 @@ pub fn main() {
     loop {
         // prompt user action
         let player_action = prompt_for_user_action(&game);
-        
+
         match player_action {
             PlayerAction::Draw => {
                 game.player_draw_card();
@@ -30,38 +31,45 @@ pub fn main() {
                     print_game_end(&game, "❌ BUSTED ❌");
                     break;
                 }
-            },
+            }
             PlayerAction::Stop => {
                 // check game state and end conditions
-                if game.dealer_score > game.player_score {
-                    print_game_end(&game, "❌ YOU LOST ❌");
-                    break;
-                } else if game.dealer_score == game.player_score {
-                    print_game_end(&game, "✋ It's a DRAW ✋");
-                    break;
-                } else {
-                    print_game_state(&game, "👀 Dealer revealer his hidden card...\n...\n...");
-                    while game.dealer_score < game.player_score {
-                        // wait 1 sec
-                        sleep();
-                        // take another card and display
-                        game.dealer_draw_card();
-                        // check endgame conditions
-                        if game.dealer_score > 21 {
-                            print_game_end(&game, "🎉 YOU WON 🎉");
-                            break;
-                        } else if game.dealer_score == game.player_score {
-                            print_game_end(&game, "✋ It's a DRAW ✋");
-                            break;
-                        } else if game.dealer_score > game.player_score {
-                            print_game_end(&game, "❌ YOU LOST ❌");
-                            break;
-                        }
-                        print_game_state(&game, "🃏 Dealer has grabbed another card... \n...\n...");
+                match game.dealer_score.cmp(&game.player_score) {
+                    Ordering::Greater => {
+                        print_game_end(&game, "❌ YOU LOST ❌");
+                        break;
                     }
-                    break;
+                    Ordering::Equal => {
+                        print_game_end(&game, "✋ It's a DRAW ✋");
+                        break;
+                    }
+                    Ordering::Less => {
+                        print_game_state(&game, "👀 Dealer revealed his hidden card...\n...\n...");
+                        while game.dealer_score < game.player_score {
+                            // wait 1 sec
+                            sleep();
+                            // take another card and display
+                            game.dealer_draw_card();
+                            // check endgame conditions
+                            if game.dealer_score > 21 {
+                                print_game_end(&game, "🎉 YOU WON 🎉");
+                                break;
+                            } else if game.dealer_score == game.player_score {
+                                print_game_end(&game, "✋ It's a DRAW ✋");
+                                break;
+                            } else if game.dealer_score > game.player_score {
+                                print_game_end(&game, "❌ YOU LOST ❌");
+                                break;
+                            }
+                            print_game_state(
+                                &game,
+                                "🃏 Dealer has grabbed another card... \n...\n...",
+                            );
+                        }
+                        break;
+                    }
                 }
-            },
+            }
             PlayerAction::Error(err_str) => println!("\n(INVALID INPUT) {}", err_str),
         }
     }
